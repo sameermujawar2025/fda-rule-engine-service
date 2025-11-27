b81227ae13030a57e983587084fd1bf1b2ec95a3
Sameer Mujawar <sameer.mujawar@taodigitalsolutions.com>
2025-11-27 13:35:48 +0530

    Cors configuration

---
+++ src/main/java/com/tao/fda/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.tao.fda.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class CorsConfig {
+
+	 @Bean
+	  public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**")
+	                        .allowedOrigins("http://localhost:5173")
+	                        .allowedMethods("*")
+	                        .allowedHeaders("*");
+	            }
+	        };
+	    }
+}
